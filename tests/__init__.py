b10122b9e3f03849c2be68c9529253f5cdf27283
Hannah Williams <hwilli72@vols.utk.edu>
2023-03-22 18:29:46 -0400

    Inital commit

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for exam_practice."""
---
+++ tests/test_exam_practice.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `exam_practice` package."""
+
+
+import unittest
+
+from exam_practice import exam_practice
+
+
+class TestExam_practice(unittest.TestCase):
+    """Tests for `exam_practice` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
